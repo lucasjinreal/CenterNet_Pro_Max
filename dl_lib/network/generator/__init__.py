017032cd1811143adeaadefee5314521b7d65ed9
jintian <jinfagang19@163.com>
2020-03-12 22:45:34 +0800

    rm dl_lib

--- dl_lib/network/generator/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
-
-from .centernet_decode import CenterNetDecoder
-from .centernet_gt import CenterNetGT
--- dl_lib/network/head/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
-
-from .centernet_deconv import CenternetDeconv
-from .centernet_head import CenternetHead
